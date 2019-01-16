7b961864ed9dba22d7d30f54b0bb02afdc136940
zhangxin516 <zhangxin1992pm@gmail.com>
2019-01-16 10:38:18 +0800

    enrich stream with another stream

--- src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/state/RidesAndFaresExercise.java
+++ src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/state/RidesAndFaresExercise.java
@@ -26,10 +26,14 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.checkpoint.ListCheckpointed;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.RichCoFlatMapFunction;
 import org.apache.flink.util.Collector;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The "Stateful Enrichment" exercise of the Flink training
@@ -41,6 +45,9 @@ import org.apache.flink.util.Collector;
  * -rides path-to-input-file
  * -fares path-to-input-file
  *
+ *  <br/>
+ *  Sln. 1: connect + flatMap. advan: no need to consider timestamp and out-of-order events problem. disad.: memory leak and overhead management
+ *  Sln. 2: join + allowLateness. advan. : easy API. disad.: can't conquer out-of-order events problem if the lateness is too high.
  */
 public class RidesAndFaresExercise extends ExerciseBase {
 	public static void main(String[] args) throws Exception {
@@ -77,17 +84,41 @@ public class RidesAndFaresExercise extends ExerciseBase {
 
 	public static class EnrichmentFunction extends RichCoFlatMapFunction<TaxiRide, TaxiFare, Tuple2<TaxiRide, TaxiFare>> {
 
+	    private Map<Long, Tuple2<TaxiRide, TaxiFare>> joinStream;
+
 		@Override
 		public void open(Configuration config) throws Exception {
-			throw new MissingSolutionException();
+			joinStream = new HashMap<>();
 		}
 
 		@Override
 		public void flatMap1(TaxiRide ride, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+		    Tuple2<TaxiRide, TaxiFare> rideFare = joinStream.get(ride.rideId);
+		    if (rideFare == null) { // new ride ID
+                rideFare = Tuple2.of(ride, null);
+                joinStream.put(ride.rideId, rideFare);
+		    } else if (rideFare.f0 == null) {  // match a ride
+		        rideFare.setField(ride, 0);
+		        out.collect(rideFare);
+		        joinStream.remove(ride.rideId);
+            } else {  // duplicated ride
+		        assert false;
+            }
 		}
 
 		@Override
 		public void flatMap2(TaxiFare fare, Collector<Tuple2<TaxiRide, TaxiFare>> out) throws Exception {
+            Tuple2<TaxiRide, TaxiFare> rideFare = joinStream.get(fare.rideId);
+            if (rideFare == null) { // new ride ID
+                rideFare = Tuple2.of(null, fare);
+                joinStream.put(fare.rideId, rideFare);
+            } else if (rideFare.f1 == null) {  // match a fare
+                rideFare.setField(fare, 1);
+                out.collect(rideFare);
+                joinStream.remove(fare.rideId);
+            } else {  // duplicated ride
+                assert false;
+            }
 		}
 	}
 }
